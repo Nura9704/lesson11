170080f33ab8d6a91d6229a8b48eee1cf50dde58
Nurasyl <nurasil9704@gmail.com>
2023-11-29 02:53:59 +0500

    lesson 12 Practice

--- app/src/main/java/kz/course/lesson11/MainActivity.kt
+++ app/src/main/java/kz/course/lesson11/MainActivity.kt
@@ -6,13 +6,16 @@ import android.os.Bundle
 import androidx.appcompat.widget.AppCompatButton
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kz.course.lesson11.model.BaseClass
+import kz.course.lesson11.model.Date
+import kz.course.lesson11.model.Name
 
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var addButton: AppCompatButton
     private lateinit var removeButton: AppCompatButton
     private lateinit var adapter: NamesAdapter
-    private var allNames = mutableListOf<String>()
+    private var generalList = mutableListOf<BaseClass>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -45,32 +48,36 @@ class MainActivity : AppCompatActivity() {
 
     @SuppressLint("NotifyDataSetChanged")
     private fun initNames() {
+        generalList.add(Date("14.09.2023"))
+        val names = mutableListOf(Name("Nurasyl"), Name("Ainura"))
+        generalList.addAll(names)
 
-        val names = mutableListOf("Nurasyl", "Ainura")
-        names.add("Alibek")
-        names.add("Orazaiym")
-        names.add("Qairat")
+        generalList.add(Date("27.09.2023"))
+        generalList.add(Name("Orazaiym"))
+        generalList.add(Name("Alibek"))
+        generalList.add(Name("Qairat"))
 
-        allNames.addAll(names)
-        allNames.addAll(names)
-        allNames.addAll(names)
+        generalList.add(Date("25.10.2023"))
+        generalList.add(Name("Harry"))
+        generalList.add(Name("Bob"))
+        generalList.add(Name("Vasya"))
 
-        adapter.setNames(allNames)
+        adapter.setList(generalList)
         adapter.notifyDataSetChanged()
     }
 
     @SuppressLint("NotifyDataSetChanged")
     private fun addName() {
-        allNames.add(allNames.size.toString())
+        //generalList.add(generalList.size.toString())
 
         adapter.notifyDataSetChanged()
     }
 
     @SuppressLint("NotifyDataSetChanged")
     private fun removeName() {
-        if (allNames.contains("Orazaiym")) {
+        /*if (allNames.contains("Orazaiym")) {
             allNames.remove("Orazaiym")
-        }
+        }*/
 
         adapter.notifyDataSetChanged()
     }
--- app/src/main/java/kz/course/lesson11/NamesAdapter.kt
+++ app/src/main/java/kz/course/lesson11/NamesAdapter.kt
@@ -5,22 +5,52 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kz.course.lesson11.model.BaseClass
+import kz.course.lesson11.model.Date
+import kz.course.lesson11.model.Name
 
-class NamesAdapter(): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    private var names: List<String> = emptyList()
+class NamesAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private var localList: List<BaseClass> = emptyList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.name_item, parent, false)
-        return NameViewHolder(view)
+        when(viewType) {
+            TYPE_NAME -> {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.name_item, parent, false)
+                return NameViewHolder(view)
+            }
+            TYPE_DATE -> {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.item_date, parent, false)
+                return NameViewHolder(view)
+            }
+            else -> {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.name_item, parent, false)
+                return NameViewHolder(view)
+            }
+        }
     }
 
     override fun getItemCount(): Int {
-        return names.size
+        return localList.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if (localList[position] is Name) {
+            return TYPE_NAME
+        } else if (localList[position] is Date) {
+            return TYPE_DATE
+        }
+        return TYPE_NAME
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val name = names[position]
-        (holder as NameViewHolder).onBind(name)
+        if (holder is NameViewHolder) {
+            val data = localList[position] as Name // java.lang.ClassCastException:
+            // kz.course.lesson11.model.Date cannot be cast to kz.course.lesson11.model.Name
+            holder.onBind(data.name)
+        } else if (holder is DateViewHolder) {
+            val data = localList[position] as Date
+            holder.onBind(data)
+        }
     }
 
     class NameViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
@@ -31,7 +61,20 @@ class NamesAdapter(): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         }
     }
 
-    fun setNames(names: List<String>) {
-        this.names = names
+    class DateViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
+        lateinit var dateTextView: TextView
+        fun onBind(date: Date) {
+            dateTextView = view.findViewById(R.id.dateTextView)
+            dateTextView.text = date.date
+        }
+    }
+
+    fun setList(localList: List<BaseClass>) {
+        this.localList = localList
+    }
+
+    companion object{
+        private const val TYPE_NAME = 0
+        private const val TYPE_DATE = 1
     }
 }
